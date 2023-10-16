80fdb2246b7ec438c93dbd02053b767cb912b2ab
15bonte <thomas.bonte95@gmail.com>
2023-10-16 12:12:30 +0200

    Update version in setup

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="cnn_framework",
-    version="0.0.6",
+    version="0.0.7",
     author="Thomas Bonte",
     author_email="thomas.bonte@mines-paristech.fr",
     description="CNN framework",
