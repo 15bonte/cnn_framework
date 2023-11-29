85f33b0266881ab9d3abbcf610daa6220c4c5e1b
15bonte <thomas.bonte95@gmail.com>
2023-11-29 11:04:03 +0100

    Protect against missing mean_std

--- src/cnn_framework/utils/metrics/ssim.py
+++ src/cnn_framework/utils/metrics/ssim.py
@@ -1,3 +1,4 @@
+from matplotlib import pyplot as plt
 from torchmetrics.image import StructuralSimilarityIndexMeasure
 
 from ..tools import UnNormalize
@@ -9,25 +10,35 @@ class SSIM(AbstractMetric):
     Structural Similarity Index Metric.
     """
 
-    def __init__(self, *args):
+    def __init__(self, *args, data_range=None):
         super().__init__(*args)
-        self.metric = StructuralSimilarityIndexMeasure().to(self.device)
+        self.metric = StructuralSimilarityIndexMeasure(data_range).to(
+            self.device
+        )
 
     def update(self, predictions, targets, _=None, mean_std=None):
-        # Expect reconstruction images to be first channels
-        # Unormalize images to use SSIM
-        nb_input_channels = predictions.shape[1]
-        un_normalize = UnNormalize(
-            mean=mean_std["mean"][:nb_input_channels],
-            std=mean_std["std"][:nb_input_channels],
-        )
-        predictions_copy, targets_copy = predictions.clone(), targets.clone()
-        for idx in range(predictions_copy.size(0)):
-            predictions_copy[idx] = un_normalize(image=predictions_copy[idx])[
-                "image"
-            ]
-            targets_copy[idx] = un_normalize(image=targets_copy[idx])["image"]
-        self.metric.update(predictions_copy, targets_copy)
+        if mean_std is not None:
+            # Expect reconstruction images to be first channels
+            # Unormalize images to use SSIM
+            nb_input_channels = predictions.shape[1]
+            un_normalize = UnNormalize(
+                mean=mean_std["mean"][:nb_input_channels],
+                std=mean_std["std"][:nb_input_channels],
+            )
+            predictions_copy, targets_copy = (
+                predictions.clone(),
+                targets.clone(),
+            )
+            for idx in range(predictions_copy.size(0)):
+                predictions_copy[idx] = un_normalize(
+                    image=predictions_copy[idx]
+                )["image"]
+                targets_copy[idx] = un_normalize(image=targets_copy[idx])[
+                    "image"
+                ]
+            self.metric.update(predictions_copy, targets_copy)
+        else:
+            self.metric.update(predictions, targets)
 
     def get_score(self):
         return self.metric.compute().item(), None
