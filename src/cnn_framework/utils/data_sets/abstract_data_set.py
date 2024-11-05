4d6b8ef565539fcaac41916804aa83b8f8ecfab3
15bonte <thomas.bonte95@gmail.com>
2024-11-05 17:40:08 +0100

    WIP G1/S vs G2

--- src/cnn_framework/utils/data_sets/abstract_data_set.py
+++ src/cnn_framework/utils/data_sets/abstract_data_set.py
@@ -110,7 +110,7 @@ class AbstractDataSet(Dataset):
             probabilities = None  # case where name is not adapted
         else:
             category = int(categories_and_probabilities[0])
-            if category == 2:
+            if category == 0:
                 category = 1
             # Are there probabilities for classes?
             if len(categories_and_probabilities) > 1 and not one_hot:
