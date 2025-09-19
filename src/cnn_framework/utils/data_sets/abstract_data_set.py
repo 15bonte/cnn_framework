19c05a796d5b327e73bc29aeba66e4f287cfbb13
15bonte <thomas.bonte95@gmail.com>
2025-09-19 12:34:46 +0200

    Protect missing category in name

--- src/cnn_framework/utils/data_sets/abstract_data_set.py
+++ src/cnn_framework/utils/data_sets/abstract_data_set.py
@@ -108,7 +108,9 @@ class AbstractDataSet(Dataset):
         """
         # Read category from name
         categories_and_probabilities = filename.split(".")[0].split("_c")[1:]
-        if len(categories_and_probabilities) == 0:
+        if len(categories_and_probabilities) == 0 or not isinstance(
+            categories_and_probabilities[0], int
+        ):
             probabilities = None  # case where name is not adapted
         else:
             category = int(categories_and_probabilities[0])
